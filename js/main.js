c5d62a7da649abbb863e06ca76908ecffcc255f0
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-04 17:35:53 +0200

    Calcolo del prezzo del biglietto

--- js/main.js
+++ js/main.js
@@ -5,20 +5,26 @@ let anniUser = parseInt(prompt("How old are you?"));
 
 console.log("How old are you?: I am " + anniUser + " years old" );
 
-let years = "How old are you?: I am " + anniUser + " years old"
+let years = "How old are you?: I am " + anniUser + " years old";
 
 document.getElementById("years").innerHTML = years;
 
 //chiedere la distanza da percorrere con il treno
 let distance = parseInt(prompt("How many km will you travel?:"));
 
-console.log = ("How many km will you travel?: My journey will be " + distance + " km long");
+console.log("How many km will you travel?: My journey will be " + distance + " km long");
 
 let journey = "How many km will you travel?: My journey will be " + distance + " km long";
 
-document.getElementById("km").innerHTML = journey
+document.getElementById("km").innerHTML = journey;
 
 //calcolare il prezzo del biglietto in base ai km da percorrere
+
+const rate = Number( 0.25 );
+
+let price = parseInt(rate * distance);
+
+console.log("The cost of your ticket is " + price + "€");
 
 //applicare lo sconto in base alla fascia d'età
 
