bc690ad6233da498e24f3094c5a84b6c84da29f0
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-04 15:40:40 +0200

    Diagramma di flusso

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// CALCOLO DEL PREZZO DEL BIGLIETTO DEL TRENO
+
+//chiedere l'età dell'utente
+
+//chiedere la distanza da percorrere con il treno
+
+//calcolare il prezzo del biglietto in base ai km da percorrere
+
+//applicare lo sconto in base alla fascia d'età
+
+//prezzo finale del biglietto
