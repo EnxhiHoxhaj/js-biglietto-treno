efd8a1b964edb725ea18acffeec85589747efab1
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-04 16:19:44 +0200

    Richiesta della distanza da percorrere

--- js/main.js
+++ js/main.js
@@ -10,6 +10,13 @@ let years = "How old are you?: I am " + anniUser + " years old"
 document.getElementById("years").innerHTML = years;
 
 //chiedere la distanza da percorrere con il treno
+let distance = parseInt(prompt("How many km will you travel?:"));
+
+console.log = ("How many km will you travel?: My journey will be " + distance + " km long");
+
+let journey = "How many km will you travel?: My journey will be " + distance + " km long";
+
+document.getElementById("km").innerHTML = journey
 
 //calcolare il prezzo del biglietto in base ai km da percorrere
 
